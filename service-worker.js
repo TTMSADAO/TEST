059497e5312408007662e50a4901eb2a9d0f0254
TTMSADAO <ping.juppy@gmail.com>
2026-03-11 11:24:48 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Service Worker สำหรับจองคิวออนไลน์ รพ.สะเดา
 // ⚠️ กฎเหล็ก: อัปเดตเวอร์ชัน CACHE_NAME ทุกครั้งที่มีการแก้โค้ดใน index.html
-const CACHE_NAME = 'sadao-queue-v2.11';
+const CACHE_NAME = 'sadao-queue-v2.12';
 
 const ASSETS_TO_CACHE = [
   './',
